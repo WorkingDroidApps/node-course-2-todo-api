e4d4dae56e63c4d62bcfe3acb3676be865158231
Keith Goddard <working.droid.apps@gmail.com>
2017-09-03 08:37:35 +0200

    Added test for /GET Todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,12 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{text: 'test object 1'}, {text:'test object 2'}];
+
 beforeEach((done)=>{
     Todo.remove({}).then(()=>{
-        done();
-    });
+       return Todo.insertMany(todos);
+    }).then(()=> done());    
 });
 
 describe('POST/todos', ()=>{
@@ -25,7 +27,7 @@ describe('POST/todos', ()=>{
                 if(err){
                   return  done(err);
                 }
-                Todo.find().then((todos)=>{
+                Todo.find({text:text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,9 +45,22 @@ describe('POST/todos', ()=>{
                 return  done(err);
               }
             Todo.find().then((todos)=>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch((e)=>done(e));
         })
     });
+});
+
+describe('GET/Todos', ()=>{
+    it('should return all Todos', (done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
+    });
+
 });
