8f16b6d00f48968a291cc2ef5965e0d93ca589cf
Keith Goddard <working.droid.apps@gmail.com>
2017-09-04 14:06:12 +0200

    Changed things to allow access thru Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -22,6 +23,10 @@ app.post('/todos', (req, res)=>{
         console.log('Unable to save Todo', e);
     });
 });
+
+// app.get('/', (req,res)=>{
+//     res.send('Hello.....');
+// });
 
 
 app.get('/todos', (req,res)=>{
@@ -50,8 +55,8 @@ app.get('/todos/:id',(req,res)=>{
 });
 
 
-app.listen(3000, ()=>{
-    console.log('Started on Port 3000');
+app.listen(port, ()=>{
+    console.log(`Started on Port ${port}`);
 });
 
 module.exports = {app};
